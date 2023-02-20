aa4c7818be5565d7ef7ec72ff3226e3fd097100b
hdavella <hdavella@gmail.com>
2023-02-20 14:22:12 -0300

    menu

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require('colors');
+const { mostrarMenu } = require('./helpers/mensajes');
+
 console.clear();
 
 const main = async()=>{
 
     console.log('Hola Mundo');
+    mostrarMenu();
 }
 
 main();
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -3,7 +3,19 @@ require('colors');
 const mostrarMenu = ()=>{
 
     console.clear();
-    console.log('==========');
-    console.log('==========');
-    console.log('==========');
+    console.log('======================='.green);
+    console.log('   Seleccione opción'.grey);
+    console.log('=======================\n'.green);
+
+    console.log(`${'1.'.green} Crear Tarea`);
+    console.log(`${'2.'.green} Listar Tareas`);
+    console.log(`${'3.'.green} Listar Tareas Completadas`);
+    console.log(`${'4.'.green} Listar Tareas Pendientes`);
+    console.log(`${'5.'.green} Completar Tarea(s)`);
+    console.log(`${'6.'.green} Borar Tarea`);
+    console.log(`${'0.'.green} Salir\n`);
+}
+
+module.exports = {
+    mostrarMenu
 }
