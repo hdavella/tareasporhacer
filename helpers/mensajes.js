70db930bf3ea13f5fdc8f980ac7f9f9ce5e2d9a1
hdavella <hdavella@gmail.com>
2023-02-22 07:16:32 -0300

    ingreso opcion

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,21 +1,30 @@
-require('colors');
+require("colors");
 
-const mostrarMenu = ()=>{
+const mostrarMenu = () => {
+  console.clear();
+  console.log("=======================".green);
+  console.log("   Seleccione opción".grey);
+  console.log("=======================\n".green);
 
-    console.clear();
-    console.log('======================='.green);
-    console.log('   Seleccione opción'.grey);
-    console.log('=======================\n'.green);
+  console.log(`${"1.".green} Crear Tarea`);
+  console.log(`${"2.".green} Listar Tareas`);
+  console.log(`${"3.".green} Listar Tareas Completadas`);
+  console.log(`${"4.".green} Listar Tareas Pendientes`);
+  console.log(`${"5.".green} Completar Tarea(s)`);
+  console.log(`${"6.".green} Borar Tarea`);
+  console.log(`${"0.".green} Salir\n`);
 
-    console.log(`${'1.'.green} Crear Tarea`);
-    console.log(`${'2.'.green} Listar Tareas`);
-    console.log(`${'3.'.green} Listar Tareas Completadas`);
-    console.log(`${'4.'.green} Listar Tareas Pendientes`);
-    console.log(`${'5.'.green} Completar Tarea(s)`);
-    console.log(`${'6.'.green} Borar Tarea`);
-    console.log(`${'0.'.green} Salir\n`);
-}
+  const ReadLine = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  ReadLine.question(`Ingrese una opción: `, (opt)=> {
+    console.log({opt});
+    ReadLine.close();
+  });
+};
 
 module.exports = {
-    mostrarMenu
-}
+  mostrarMenu,
+};
