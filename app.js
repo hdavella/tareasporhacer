9ae5c4ce7aed391fbc60a604a8d424de65d34f43
hdavella <hdavella@gmail.com>
2023-03-01 12:19:22 -0300

    class tareas

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 
 //console.clear();
@@ -6,11 +8,22 @@ const { inquirerMenu, pausa } = require('./helpers/inquirer');
 const main = async () => {
 
     let opt = '';
+    const tareas = new Tareas();
+    
     do {
+        /*      const tareas = new Tareas();
+                const tarea = new Tarea('Pinta soporte');
+                
+                tareas._listado [tarea.id] = tarea;
+        
+                console.log (tareas) */
 
         opt = await inquirerMenu();
-        console.log({opt});
-        if(opt !== '0') await pausa();
+        console.log({ opt });
+
+
+
+        if (opt !== '0') await pausa();
 
     } while (opt !== '0')
 }
